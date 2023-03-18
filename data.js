ca43bdf2802551f46291b3f7e7bf5d09218ecd84
rmzvr <rmzvr@proton.me>
2023-03-18 10:30:02 +0100

    update

---
+++ data.js
@@ -0,0 +1,97 @@
+export const content_row1 = [
+  {
+    name: 'stats preview',
+    url: 'https://rmzvr.github.io/stats-preview-card/',
+    image: '/images/1.webp',
+  },
+  {
+    name: 'sunnyside agency',
+    url: 'https://rmzvr.github.io/sunnyside-agency-landing-page/',
+    image: '/images/2.webp',
+  },
+  {
+    name: 'calculator app',
+    url: 'https://rmzvr.github.io/calculator-app/',
+    image: '/images/4.webp',
+  },
+  {
+    name: 'faq accordion',
+    url: 'https://rmzvr.github.io/faq-accordion-card/',
+    image: '/images/3.webp',
+  },
+  {
+    name: 'url shortening',
+    url: 'https://rmzvr.github.io/url-shortening-api/',
+    image: '/images/5.webp',
+  },
+  {
+    name: 'todo app',
+    url: 'https://rmzvr.github.io/todo-app/',
+    image: '/images/7.webp',
+  },
+  {
+    name: 'interactive comments',
+    url: 'https://rmzvr.github.io/interactive-comments-section/',
+    image: '/images/6.webp',
+  },
+]
+
+export const content_row2 = [
+  {
+    name: 'Planet facts',
+    url: 'https://rmzvr.github.io/planets-fact/',
+    image: '/images/10.webp',
+  },
+  {
+    name: 'officelite landing',
+    url: 'https://rmzvr.github.io/officelite-coming-soon-site/',
+    image: '/images/8.webp',
+  },
+  {
+    name: 'Rock, paper, scissors game',
+    url: 'https://rmzvr.github.io/rock-paper-scissors-game/',
+    image: '/images/11.webp',
+  },
+  {
+    name: 'Random landing',
+    url: 'https://rmzvr.github.io/frontend-science/',
+    image: '/images/9.webp',
+  },
+  {
+    name: 'Shipping tracking',
+    url: 'https://rmzvr.github.io/shipping_tracking_app/',
+    image: '/images/12.webp',
+  },
+  {
+    name: 'Trello (clone) app',
+    url: 'https://rmzvr.github.io/shipping_tracking_app/',
+    image: '/images/13.webp',
+  },
+]
+
+export const socials = [
+  {
+    url: 'https://www.linkedin.com/in/rmzvr/',
+    image: '/images/linkedin.svg',
+  },
+  {
+    url: 'https://github.com/rmzvr',
+    image: '/images/github.svg',
+  },
+  {
+    url: 'https://t.me/rmzvr/',
+    image: '/images/telegram.svg',
+  },
+  {
+    url: 'https://join.skype.com/invite/IT8NGMAUZXDb',
+    image: '/images/skype.svg',
+  },
+  {
+    url: 'mailto:rmzvr@proton.me',
+    image: '/images/protonmail.svg',
+  },
+  {
+    url: 'https://media.licdn.com/dms/document/C4E2DAQFItj0EXBARYQ/profile-treasury-document-pdf-analyzed/0/1679131498689?e=1680134400&v=beta&t=1hUQcYLPpAZq8LY_-QGfLZO2L9qXkXCIXprdgcvTt-A',
+    image: '/images/resume2.png',
+  },
+]
--- main.js
+++ main.js
@@ -1,215 +1,119 @@
+import { content_row1, content_row2, socials } from './data'
+
 window.onload = () => {
-	const content1 = [
-		{
-			name: 'stats preview',
-			url: 'https://rmzvr.github.io/stats-preview-card/',
-			image: '/images/1.webp'
-		},
-		{
-			name: 'sunnyside agency',
-			url: 'https://rmzvr.github.io/sunnyside-agency-landing-page/',
-			image: '/images/2.webp'
-		},
-		{
-			name: 'calculator app',
-			url: 'https://rmzvr.github.io/calculator-app/',
-			image: '/images/4.webp'
-		},
-		{
-			name: 'faq accordion',
-			url: 'https://rmzvr.github.io/faq-accordion-card/',
-			image: '/images/3.webp'
-		},
-		{
-			name: 'url shortening',
-			url: 'https://rmzvr.github.io/url-shortening-api/',
-			image: '/images/5.webp'
-		},
-		{
-			name: 'todo app',
-			url: 'https://rmzvr.github.io/todo-app/',
-			image: '/images/7.webp'
-		},
-		{
-			name: 'interactive comments',
-			url: 'https://rmzvr.github.io/interactive-comments-section/',
-			image: '/images/6.webp'
-		}
-	]
+  window.addEventListener('resize', () => {
+    fillText(3, initialCell, 'projects')
+    fillContent(3, initialCell, content_row1)
+    fillContent(4, initialCell + 1, content_row2)
 
-	const content2 = [
-		{
-			name: 'Planet facts',
-			url: 'https://rmzvr.github.io/planets-fact/',
-			image: '/images/10.webp'
-		},
-		{
-			name: 'officelite landing',
-			url: 'https://rmzvr.github.io/officelite-coming-soon-site/',
-			image: '/images/8.webp'
-		},
-		{
-			name: 'Rock, paper, scissors game',
-			url: 'https://rmzvr.github.io/rock-paper-scissors-game/',
-			image: '/images/11.webp'
-		},
-		{
-			name: 'Random landing',
-			url: 'https://rmzvr.github.io/frontend-science/',
-			image: '/images/9.webp'
-		},
-		{
-			name: 'Shipping tracking',
-			url: 'https://rmzvr.github.io/shipping_tracking_app/',
-			image: '/images/12.webp'
-		},
-		{
-			name: 'Trello (clone) app',
-			url: 'https://rmzvr.github.io/shipping_tracking_app/',
-			image: '/images/13.webp'
-		}
-	]
+    fillText(6, initialCell + 1, 'socials')
+    fillSocials(7, initialCell + 2, socials)
+  })
 
-	const socials = [
-		{
-			url: 'https://www.linkedin.com/in/rmzvr/',
-			image: '/images/linkedin.svg'
-		},
-		{
-			url: 'https://github.com/rmzvr',
-			image: '/images/github.svg'
-		},
-		{
-			url: 'https://t.me/rmzvr/',
-			image: '/images/telegram.svg'
-		},
-		{
-			url: 'https://join.skype.com/invite/IT8NGMAUZXDb',
-			image: '/images/skype.svg'
-		},
-		{
-			url: 'mailto:rmzvr@proton.me',
-			image: '/images/protonmail.svg'
-		},
-		{
-			url: 'https://media.licdn.com/dms/document/C4E2DAQFRUaPw1h2uXQ/profile-treasury-document-pdf-analyzed/0/1672066223846?e=1675900800&v=beta&t=H_-Fgw1t7uuC0MOYPc73e_GKxExlYLsPl7uH-Pu_Evg',
-			image: '/images/resume2.png'
-		}
-	]
+  createGrid()
+  const rows = document.querySelectorAll('.row')
 
-	window.addEventListener('resize', () => {
-		fillText(3, initialCell, 'projects')
-		fillContent(3, initialCell, content1)
-		fillContent(4, initialCell + 1, content2)
+  let initialCell = Math.round(
+    (rows[0].children.length - 'projects'.length) / 2
+  )
 
-		fillText(6, initialCell + 1, 'socials')
-		fillSocials(7, initialCell + 2, socials)
-	})
+  initialCell < 2 ? (initialCell = 2) : null
 
-	createGrid()
-	const rows = document.querySelectorAll('.row')
+  fillText(3, initialCell, 'projects')
+  fillContent(3, initialCell, content_row1)
+  fillContent(4, initialCell + 1, content_row2)
 
-	let initialCell = Math.round(
-		(rows[0].children.length - 'projects'.length) / 2
-	)
-
-	initialCell < 2 ? (initialCell = 2) : null
-
-	fillText(3, initialCell, 'projects')
-	fillContent(3, initialCell, content1)
-	fillContent(4, initialCell + 1, content2)
-
-	fillText(6, initialCell, 'socials')
-	fillSocials(7, initialCell + 1, socials)
+  fillText(6, initialCell, 'socials')
+  fillSocials(7, initialCell + 1, socials)
 }
 
 function createGrid() {
-	for (let i = 0; i < Math.round(window.innerHeight / 77 + 1); i++) {
-		const row = document.createElement('div')
+  for (let i = 0; i < Math.round(window.innerHeight / 77 + 1); i++) {
+    const row = document.createElement('div')
 
-		row.classList.add('row')
+    row.classList.add('row')
 
-		for (let i = 0; i < Math.round(window.innerWidth / 96 + 1); i++) {
-			const hexagon = document.createElement('a')
+    for (let i = 0; i < Math.round(window.innerWidth / 96 + 1); i++) {
+      const hexagon = document.createElement('a')
 
-			hexagon.classList.add('hexagon')
+      hexagon.classList.add('hexagon')
 
-			row.appendChild(hexagon)
-		}
+      row.appendChild(hexagon)
+    }
 
-		document.querySelector('.container').appendChild(row)
-	}
+    document.querySelector('.container').appendChild(row)
+  }
 }
 
 function fillText(row, initialCell, text) {
-	const rows = document.querySelectorAll('.row')
-	const letters = text.toUpperCase().split('')
+  const rows = document.querySelectorAll('.row')
+  const letters = text.toUpperCase().split('')
 
-	letters.forEach((letter) => {
-		const currentCell = rows[row - 1].children[initialCell - 1]
+  letters.forEach((letter) => {
+    const currentCell = rows[row - 1].children[initialCell - 1]
 
-		initialCell++
+    initialCell++
 
-		currentCell.innerHTML = `<span style="font-size: 36px">${letter}</span>`
-	})
+    currentCell.innerHTML = `<span style="font-size: 36px">${letter}</span>`
+  })
 }
 
 function fillSocials(row, initalCell, content) {
-	const rows = document.querySelectorAll('.row')
+  const rows = document.querySelectorAll('.row')
 
-	content.forEach((item, index) => {
-		const currEl = rows[row - 1].children[initalCell - 1]
+  content.forEach((item, index) => {
+    const currEl = rows[row - 1].children[initalCell - 1]
 
-		initalCell++
+    initalCell++
 
-		currEl.target = '_blank'
-		currEl.style.cursor = 'pointer'
-		currEl.innerHTML = `
+    currEl.target = '_blank'
+    currEl.style.cursor = 'pointer'
+    currEl.innerHTML = `
       <a href="${content[index].url}"
       target="_blank"
       style="width: 100%; height: 100%; display: grid; place-content: center;">
         <img width="50" heigth="50" src="${content[index].image}" alt="icon">
       </a>
     `
-	})
+  })
 }
 
 function fillContent(row, initalCell, content) {
-	const rows = document.querySelectorAll('.row')
+  const rows = document.querySelectorAll('.row')
 
-	content.forEach((item, index) => {
-		const currEl = rows[row].children[initalCell - 1]
+  content.forEach((item, index) => {
+    const currEl = rows[row].children[initalCell - 1]
 
-		initalCell++
+    initalCell++
 
-		currEl.target = '_blank'
-		currEl.classList.add('active')
-		currEl.innerHTML = `
+    currEl.target = '_blank'
+    currEl.classList.add('active')
+    currEl.innerHTML = `
       <div>
         <span>
           ${content[index].name.split('-').join(' ')}
         </span>
       </div>
       `
-		currEl.style.cursor = 'pointer'
-		currEl.style.backgroundImage = `url(${content[index].image})`
+    currEl.style.cursor = 'pointer'
+    currEl.style.backgroundImage = `url(${content[index].image})`
 
-		currEl.addEventListener('click', (e) => {
-			if (e.target.classList.contains('open')) {
-				e.target.href = content[index].url
-			}
+    currEl.addEventListener('click', (e) => {
+      if (e.target.classList.contains('open')) {
+        e.target.href = content[index].url
+      }
 
-			e.target.classList.add('open')
+      e.target.classList.add('open')
 
-			e.target.style.transform = 'scale(4)'
-		})
+      e.target.style.transform = 'scale(4)'
+    })
 
-		currEl.addEventListener('mouseleave', (e) => {
-			e.target.style.transform = 'scale(1)'
-			e.target.removeAttribute('href')
-			setTimeout(() => {
-				e.target.classList.remove('open')
-			}, 300)
-		})
-	})
+    currEl.addEventListener('mouseleave', (e) => {
+      e.target.style.transform = 'scale(1)'
+      e.target.removeAttribute('href')
+      setTimeout(() => {
+        e.target.classList.remove('open')
+      }, 300)
+    })
+  })
 }
